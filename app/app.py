1aca3a82907642e6fe7095bb00d8f3fedc409796
Neda <neda.sultova@gmail.com>
2025-01-08 15:30:02 +0100

    most basic PoC working

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
+import os
 from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/")
-def index_page():
-    return render_template("index.html")
 
+UPLOAD_FOLDER = 'static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def index_page():
+    # Get list of images from upload directory
+    images = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        images.append(filename)
+    return render_template('index.html', images=images)
